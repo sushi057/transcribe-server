5c552649dc6777104ebd3d30feb1e9c51d661266
Suvash Joshi <suvash2077@gmail.com>
2024-04-10 16:15:52 +0545

    fix: install dependencies

--- utils.py
+++ utils.py
@@ -59,11 +59,11 @@
 # Clean the single audio file by clipping silent gaps from both ends
 # def clean_single_wav(wav, win_size=500):
 
-'''
+"""
 Important Notes:
     1. The dimension of wav files is in the format of  -> batch * sequence * frame_size.
 
-'''
+"""
 
 
 from tensorflow.keras import models
@@ -80,7 +80,7 @@ import edit_distance as ed
 # Global configs required for training
 from configs import INPUT_DIM, SR, N_MFCC, HOP_LENGTH, FRAME_SIZE
 
-device_name = '/device:CPU:0'
+device_name = "/device:CPU:0"
 
 # U.0
 # Loads model from the directory argument
@@ -89,6 +89,7 @@ device_name = '/device:CPU:0'
 def load_model(model_dir):
     return models.load_model(model_dir)
 
+
 # U.1
 # Loading wav file from librosa
 
@@ -96,13 +97,17 @@ def load_model(model_dir):
 def load_wav(dir):
     return librosa.load(dir, sr=SR)[0]
 
+
 # U.2
 # Generates Normalized MFCCs from audio
 
 
 def gen_mfcc(arr):
-    mfccs = librosa.feature.mfcc(
-        y=arr[:-1], sr=SR, n_mfcc=N_MFCC, hop_length=HOP_LENGTH).transpose().flatten()
+    mfccs = (
+        librosa.feature.mfcc(y=arr[:-1], sr=SR, n_mfcc=N_MFCC, hop_length=HOP_LENGTH)
+        .transpose()
+        .flatten()
+    )
     return (mfccs - np.mean(mfccs)) / np.std(mfccs)
 
 
@@ -119,12 +124,14 @@ def pad_text(list_texts, unq_chars, unk_idx=1):
 
         # index 1 for the unknown chars
         arr = np.array(
-            [unq_chars.index(ch) if ch in unq_chars else unk_idx for ch in txt])
+            [unq_chars.index(ch) if ch in unq_chars else unk_idx for ch in txt]
+        )
 
         padded_arr.append(arr)
         seq_lengths.append(len_seq)
 
     return np.array(padded_arr), np.array(seq_lengths)
+
 
 # U.4
 # Returns tensor batch*seq*frame
@@ -145,6 +152,7 @@ def pad_list_np(list_np):
 
     return np.array(padded_arr).reshape((len(list_np), -1, INPUT_DIM))
 
+
 # U.5
 # Generates batches of wavs and texts  with padding as per needed
 
@@ -154,10 +162,14 @@ def batchify(wavs, texts, unq_chars):
     # generates tensor of dim (batch * seq * frame)
     input_tensor = pad_list_np(wavs)
     target_tensor, target_lengths_tensor = pad_text(texts, unq_chars)
-    output_seq_lengths_tensor = np.full(
-        (len(wavs),), fill_value=input_tensor.shape[1])
+    output_seq_lengths_tensor = np.full((len(wavs),), fill_value=input_tensor.shape[1])
 
-    return input_tensor, target_tensor, target_lengths_tensor.reshape((-1, 1)), output_seq_lengths_tensor.reshape((-1, 1))
+    return (
+        input_tensor,
+        target_tensor,
+        target_lengths_tensor.reshape((-1, 1)),
+        output_seq_lengths_tensor.reshape((-1, 1)),
+    )
 
 
 # U.6
@@ -172,27 +184,29 @@ def plot_losses(dir, optimal_epoch=None):
     epochs = len(train_losses)
     # print(len(test_losses))
 
-    X = range(1, epochs+1)
+    X = range(1, epochs + 1)
 
     fig, ax = plt.subplots(1, figsize=(15, 10))
 
-    fig.suptitle('Train and Test Losses', fontsize=25,)
+    fig.suptitle(
+        "Train and Test Losses",
+        fontsize=25,
+    )
 
     ax.set_xlim(0, 72)
     ax.plot(X, train_losses, color="red", label="Train Loss")
     ax.plot(X, test_losses, color="green", label="Test Loss")
 
-    plt.rcParams.update({'font.size': 20})
+    plt.rcParams.update({"font.size": 20})
 
     plt.legend(loc="upper right", frameon=False, fontsize=20)
     # plt.xlabel("Epochs",{"size":20})
     # plt.ylabel("Loss", {"size":20})
 
-    if (optimal_epoch != None):
+    if optimal_epoch != None:
         plt.axvline(x=optimal_epoch, ymax=0.5)
         # ax.plot(58, 0, 'go', label='marker only')
-        plt.text(optimal_epoch, 35,
-                 f'Optimal Epoch at {optimal_epoch}', fontsize=15)
+        plt.text(optimal_epoch, 35, f"Optimal Epoch at {optimal_epoch}", fontsize=15)
 
     plt.show()
 
@@ -206,8 +220,9 @@ def predict_from_mfccs(model, mfccs, unq_chars):
 
     input_len = np.ones(pred.shape[0]) * pred.shape[1]
     # Use greedy search. For complex tasks, you can use beam search
-    results = K.ctc_decode(pred, input_length=input_len,
-                           greedy=False, beam_width=100)[0][0]
+    results = K.ctc_decode(pred, input_length=input_len, greedy=False, beam_width=100)[
+        0
+    ][0]
 
     sentences = []
     char_indices = []
@@ -231,6 +246,7 @@ def predict_from_wavs(model, wavs, unq_chars):
     mfccs = [gen_mfcc(wav) for wav in wavs]
     return predict_from_mfccs(model, mfccs, unq_chars)
 
+
 # U.9
 # Converts the text to list of indices as per the unique characters list
 
@@ -241,17 +257,16 @@ def indices_from_texts(texts_list, unq_chars, unk_idx=1):
     for txt in texts_list:
 
         # index 1 for the unknown chars
-        lst = [unq_chars.index(
-            ch) if ch in unq_chars else unk_idx for ch in txt]
+        lst = [unq_chars.index(ch) if ch in unq_chars else unk_idx for ch in txt]
 
         indices_list.append(lst)
 
     return indices_list
 
 
-'''
+"""
 Calculates CER( character error rate) from dataset;
-'''
+"""
 # U.10
 # CER from mfccs
 
@@ -272,7 +287,8 @@ def CER_from_mfccs(model, mfccs, texts, unq_chars, batch_size=100):
             else:
                 end = len_mfccs
             pred_sentences, pred_indices = predict_from_mfccs(
-                model, mfccs[start:end], unq_chars)
+                model, mfccs[start:end], unq_chars
+            )
             actual_indices = indices_from_texts(texts[start:end], unq_chars)
 
             len_batch_texts = end - start
@@ -290,12 +306,19 @@ def CER_from_mfccs(model, mfccs, texts, unq_chars, batch_size=100):
             batch_count += 1
             sum_cer += batch_cer
 
-            print("CER -> {:.2f}%, \t No.of sentences -> {}, \t Time Taken -> {:.2f} secs.".format(
-                (sum_cer / batch_count) * 100, end, time.time() - start_time))
+            print(
+                "CER -> {:.2f}%, \t No.of sentences -> {}, \t Time Taken -> {:.2f} secs.".format(
+                    (sum_cer / batch_count) * 100, end, time.time() - start_time
+                )
+            )
 
         print(
-            "The total time taken for all sentences CER calculation is  {:.2f} secs.".format(time.time() - start_time))
+            "The total time taken for all sentences CER calculation is  {:.2f} secs.".format(
+                time.time() - start_time
+            )
+        )
         return sum_cer / batch_count
+
 
 # U.11
 # CER from wavs
@@ -319,6 +342,7 @@ def ctc_softmax_output_from_mfccs(model, mfccs):
     y = model(mfccs)
     return y
 
+
 # U.13
 # CTC softmax probabilities output from wavs
 
@@ -333,14 +357,14 @@ def ctc_softmax_output_from_wavs(model, wavs):
 def clean_single_wav(wav, win_size=500):
     wav_avg = np.average(np.absolute(wav))
 
-    for s in range(0, len(wav)-win_size, win_size):
-        window = wav[s:s+win_size]
+    for s in range(0, len(wav) - win_size, win_size):
+        window = wav[s : s + win_size]
         if np.average(np.absolute(window)) > wav_avg:
             wav = wav[s:]
             break
 
-    for s in range(len(wav)-win_size, 0, -win_size):
-        window = wav[s-win_size:s]
+    for s in range(len(wav) - win_size, 0, -win_size):
+        window = wav[s - win_size : s]
         if np.average(np.absolute(window)) > wav_avg:
             wav = wav[:s]
             break
